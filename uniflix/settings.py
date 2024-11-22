36762164c750f3931e9837b28f769f8421831020
celovieiras <celovieirads@gmail.com>
2024-11-21 23:34:17 -0300

    Revert "Adicionando vercel.json"
    This reverts commit 53951c756703a132f0b17070e8a242d14eb5b854.

--- uniflix/settings.py
+++ uniflix/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 from django.conf.global_settings import AUTH_USER_MODEL, LOGIN_REDIRECT_URL
 
@@ -28,13 +27,12 @@ SECRET_KEY = "django-insecure-x^l0ooeyk)tu53h5)j0g(8$b@@68790pgap#iq2%s&lh6#bj-w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    "whitenoise.runserver_nostatic",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -48,7 +46,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -130,9 +127,6 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-
-STATIC_DIRS = [ os.path.join(BASE_DIR, 'static') ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
